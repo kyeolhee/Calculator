75bb664f1c6389777928d2b7386ef0a6134c1275
scsc0511 <scsc0511@naver.com>
2016-05-31 02:27:31 +0900

    Add files via upload

--- input.c
+++ input.c
@@ -1,50 +1,58 @@
 #include <stdio.h>
+#include <string.h>
+char input[10000][60], pass_operand[10000][60], dot[10000];
 int main()
 {
-	int i, j, dot;
-	char input[60]={0}, operand[60]={0};
-	scanf("%s", input);
-
-
-	//input
-	for(i = 0; i <= 59; i++)
-		printf("input[%2d] = %c\n", i, input[i]);
-	
-
-	//dot 위치
-	for(i = 0; i <= 59; i++)
-		if(input[i] == '.') dot = i;
-	operand[50] = input[dot];
-	
-	
-	//정수 위치 배정
-	for(i = dot - 1, j = 49; j >= 0; i--, j--)
-	{
-		operand[j] = input[i];
-		if(input[i] >= 48 && input[i] <= 57);
-		else operand[j] = '0';
-		if(i < 0) operand[j] = '0';		//쓰레기값 제거
-	}
-	//소수 위치 배정
-	for(i = dot + 1, j = 51; j <= 59; i++, j++)
-	{
-		operand[j] = input[i];
-		if(input[i] >= 48 && input[i] <= 57);
-		else operand[j] = '0';
-	}
-
-
-	//배열 하나하나 출력
-	for(i = 0; i <= 59; i++)
-		printf("operand[%2d] = %c\n", i, operand[i]);
-
-		
-	//배정한 숫자 출력(자리수 고려)
-	printf("operand : ");
-	for(i = 0; i <= 59; i++)
-		printf("%c", operand[i]);
-	printf("\n");
-
+	int i, j, k,l,m,n,o,p,q,r,s,t,u,end;
+	char c;
+	for(int i=0;i<=10000;i++)
+		{scanf("%s",input[i]);
+		 if((c=getchar())=='\n')
+			 break;
+		}
+	for(j=0;j<=10000;j++)
+		if(strlen(input[j])==0)
+			{end=j-1;
+			 break;
+		 	}	 
+	for(int k=0;k<=end;k++)				//소수점의 위치 찾기 + 소수점의 위치 배정 (소수점이 있는경우 >> 입력된 숫자가 정수가 아닌 실수인 경우)
+		for(l = 0;l<= 60;l++)
+			{if(input[k][l] == '.') 
+				{dot[k] = l;
+			 	 pass_operand[k][50] = input[k][dot[k]];
+				}
+			}
+	for(m=0;m<=end;m++)
+		{if(pass_operand[m][50]=='.')						//입력된 값이 정수가 아닌 실수인 경우의 정수부 위치 배정
+			{for(n = dot[m] - 1, o = 49; o >= 0; n--, o--)
+				{if(n>=0)
+				 	pass_operand[m][o] = input[m][n];
+				 else if (n < 0)
+				 	pass_operand[m][o] = '0';	 
+				}
+			 for(t = 0; t<=end ; t++)								//소수 위치 배정
+				for(r = dot[t] + 1, s = 51; s <= 59; r++, s++)
+					{if(input[t][r] >= '0' && input[t][r] <= '9')
+					 	pass_operand[t][s] = input[t][r];
+		 	 	 	 else 
+					 	pass_operand[t][s] = '0';
+					}
+			}
+		else if(pass_operand[j][50]!='.')					//입력된 값이 정수인 경우의 정수부 위치 배정
+			for(n=strlen(input[m])-1, o=49;o>=0;n--,o--)
+				{if(n>=0)
+				 	pass_operand[m][o] = input[m][n];
+				 else if (n < 0)
+				 	pass_operand[m][o] = '0';	 
+			 	 if(input[m][n] >= '0' && input[m][n] <= '9');
+			 	 else 
+					 pass_operand[m][o] = '0';
+				}
+			 for(u=51;u<=59;u++)
+				 pass_operand[m][u]='0';
+		}
+	for(int i=0;i<= end;i++)							//왜 전체 숫자가 출력? 
+		for(int j=0;j<=60;j++)
+			printf("%c\n",pass_operand[i][j]);
 	return 0;
-
 }
