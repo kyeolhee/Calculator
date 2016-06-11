0a94d6c370bceac56fe57d2f4dce00d3a80175c3
kyeolhee <ggood929@naver.com>
2016-06-12 00:38:08 +0900

    Update var_Input.c
    변수 완성

--- var_Input.c
+++ var_Input.c
@@ -1,49 +1,75 @@
 #include <stdio.h>
 #include <string.h>
-int main ()
+void cal()
 {
-        char a[10000][61] = {0};
-        char c = 'a';
-        char var[10][61] = {'@'};
+        char a[10000][62] = {0}, input[10000][62]={0};
+        char b[10000][62] = {0};
+        char c;
+        char var[10][62] = {0}; // 전역변수로
         char tmp[100] = {0};
-        int i, j, k, m;
-        char gap;
-        scanf("%[^\n]", a[0]); // 개행 되기 전까지 scanf로 받음
-        for(;c<='z'; c++)
+        int i, j, k, m, end;
+        int gap;
+        // 수식 받기
+        for(i=0;i<=10000;i++)
+        {
+                scanf("%s",a[i]);
+                if((c=getchar())=='\n')
+                {
+                        end=i;
+                        break;
+                }
+        }
+
+        // 변수를 받는 식인지 구분
+        for(c = 'A'; c<='z'; c++)
         {
                 strcpy(tmp,"? = ");
                 tmp[0] = c; // tmp의 첫번째 배열이 a부터 z까지 바뀜
-                if (strncasecmp(a[0], tmp, 3) == 0) // 배열 a와 tmp가 앞으ㅔ 3자리가 같으면 strncasecmp의 값음 0
+                if (a[0][0] == c && a[1][0] == '=')
                 {
-                        for (j = 0; j <= 9; j++)
+                        for (k = 0; k <= 61; k++)
                         {
-                                if (var[j][0] == '@')
+                                if (a[2][k] >= '0' && a[2][k] <= '9' || a[2][k] == 0) // 변수의 값에 입력되는 것이 특수문자가 아닐때
                                 {
-                                        // + 특수문자가 들어가면 오류뜨게
-                                        var[j][0] = c;
-                                        for (i = 1; i <= 61; i++)
-                                                var[j][i] = a[0][i+3];
+                                        m = 0;
+                                }
+                                else
+                                {
+                                        m = 1;
                                         break;
-                                        //for문을 빠져나옴
                                 }
                         }
-                        // 변수에 저장
-                        for (i = 4; i<=61; i++) // 이경우에 면수는 자릴수 57자리까지 받음 수정해야할듯
+                        // 변수 저장
+                        if (m == 0)
                         {
-                                printf("%c", a[0][i]);
+                                for (j = 0; j <= 9; j++)
+                                {
+                                        if (var[j][0] == 0)
+                                        {
+                                                var[j][0] = c;
+                                                for (i = 1; i <= 61; i++)
+                                                {
+                                                        var[j][i] = a[2][i-1];
+                                                }
+                                                break;
+                                        }
+                                }
+
+                                // 입력된 변수의 값 출력
+                                printf("= ");
+                                for (i = 0; i <= 61; i++)
+                                        printf("%c", a[2][i]);
+                                printf("\n");
                         }
-                        // 화면에 변수값 출력
+                        else if (m == 1)
+                                printf("= error\n");
                 }
         }
-	gap = 'A' - 'a';
-        for (i = 0; i <= 61; i++)
-                for (j = 0; j <= 61; j++)
-                        if (a[i][j] <= 'z' && a[i][j] >= 'A')
-                                for (k = 0; k <= 9; k++)
-                                         if (a[i][j] == var[k][0] || a[i][j] + gap == var[k][0] || a[i][j] - gap == var[k][0])
-                                                for (m = j; m <= 61; m++)
-                                                        a[i][m] = var[k][m-j+1];
-        // 입력된 배열이 알파벳이면 그 알파벳을 var 배열의 0번째와 비교해서 맞으면 var배열의 1번째 값부터 배열에 다시 넣는다
+        return;
+}
+int main ()
+{
+        cal();
+        main ();
         return 0;
 }
-                         
